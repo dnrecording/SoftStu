394f63dbbba8ea022c4346ae04008ecd65cd23f8
poksiwakorn <62551895+poksiwakorn@users.noreply.github.com>
2022-02-27 22:40:16 +0700

    Woohoo

--- HW5/assignment/src/App.js
+++ HW5/assignment/src/App.js
@@ -38,12 +38,12 @@ function App() {
         {/* Personal card */}
         <div class="flex flex-col bg-white font-semibold text-center rounded-3xl shadow-lg p-7 max-w-3xl my-5">
           <img
-            class="mb-3 w-48 h-48 rounded-full shadow-lg mx-auto"
-            src="https://sv1.picz.in.th/images/2022/01/29/nXUVB0.jpg"
+            class="mb-3 w-48 h-48 rounded-full shadow-lg mx-auto object-cover"
+            src="https://sv1.picz.in.th/images/2022/01/30/npzB6Q.jpg"
             alt="product designer"
           ></img>
-          <h1 class="text-lg text-gray-800"> Weeravit Srisuntikanaporn </h1>
-          <h3 class="text-sm text-gray-600 "> ID: 62010838 </h3>
+          <h1 class="text-lg text-gray-800"> Kanittha Sasipreyajun </h1>
+          <h3 class="text-sm text-gray-600 "> ID: 61010014 </h3>
           <p class="text-xs text-gray-400 mt-4 max-w-3xl">
             {" "}
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
@@ -76,12 +76,13 @@ function App() {
         {/* Personal card */}
         <div class="flex flex-col bg-white font-semibold text-center rounded-3xl shadow-lg p-7 max-w-3xl my-5">
           <img
-            class="mb-3 w-48 h-48 rounded-full shadow-lg mx-auto"
-            src="https://sv1.picz.in.th/images/2022/01/29/nXUVB0.jpg"
+            class="mb-3 w-48 h-48 rounded-full shadow-lg mx-auto object-cover"
+            src="https://sv1.picz.in.th/images/2022/02/06/nMEDfv.png"
             alt="product designer"
+            style={{"object-position": "center 5px"}}
           ></img>
-          <h1 class="text-lg text-gray-800"> Weeravit Srisuntikanaporn </h1>
-          <h3 class="text-sm text-gray-600 "> ID: 62010838 </h3>
+          <h1 class="text-lg text-gray-800"> Pubodin Tieanthong </h1>
+          <h3 class="text-sm text-gray-600 "> ID: 62010711 </h3>
           <p class="text-xs text-gray-400 mt-4 max-w-3xl">
             {" "}
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
@@ -114,9 +115,10 @@ function App() {
         {/* Personal card */}
         <div class="flex flex-col bg-white font-semibold text-center rounded-3xl shadow-lg p-7 max-w-3xl my-5">
           <img
-            class="mb-3 w-48 h-48 rounded-full shadow-lg mx-auto"
+            class="mb-3 w-48 h-48 rounded-full shadow-lg mx-auto object-cover"
             src="https://sv1.picz.in.th/images/2022/01/29/nXUVB0.jpg"
             alt="product designer"
+            style={{"object-position": "center 5px"}}
           ></img>
           <h1 class="text-lg text-gray-800"> Weeravit Srisuntikanaporn </h1>
           <h3 class="text-sm text-gray-600 "> ID: 62010838 </h3>
@@ -134,14 +136,53 @@ function App() {
           <div class="flex justify-evenly items-center max-w-3xl">
             <button
               class="bg-emerald-400 w-24 h-10 rounded-3xl text-gray-100 font-semibold uppercase tracking-wide mr-2"
-              onClick={() => vote(1)}
+              onClick={() => vote(2)}
             >
               Vote
             </button>
-            <h2 class="text-2xl font-bold text-slate-900">{votingCount[1]}</h2>
+            <h2 class="text-2xl font-bold text-slate-900">{votingCount[2]}</h2>
             <button
               class="bg-red-500 w-24 h-10 rounded-3xl text-gray-100 font-semibold uppercase tracking-wide ml-2"
-              onClick={() => unvote(1)}
+              onClick={() => unvote(2)}
+            >
+              Unvote
+            </button>
+          </div>
+        </div>
+        {/* end Personal card */}
+
+        {/* Personal card */}
+        <div class="flex flex-col bg-white font-semibold text-center rounded-3xl shadow-lg p-7 max-w-3xl my-5">
+          <img
+            class="mb-3 w-48 h-48 rounded-full shadow-lg mx-auto object-cover"
+            src="https://sv1.picz.in.th/images/2022/01/30/np0ESb.jpg"
+            alt="product designer"
+            style={{"object-position": "center -15px"}}
+          ></img>
+          <h1 class="text-lg text-gray-800"> Siwakon Phetnoi </h1>
+          <h3 class="text-sm text-gray-600 "> ID: 62010882 </h3>
+          <p class="text-xs text-gray-400 mt-4 max-w-3xl">
+            {" "}
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+            facilisis lectus quis dui eleifend egestas. Pellentesque habitant
+            morbi tristique senectus et netus et malesuada fames ac turpis
+            egestas. Sed ac urna venenatis, consectetur elit ac, volutpat ipsum.
+            Aliquam placerat suscipit volutpat. Mauris quis congue diam. Sed at
+            lacus nec est molestie interdum. Donec scelerisque elementum auctor.
+            Integer quis justo accumsan, ullamcorper tellus id, rutrum ipsum.{" "}
+          </p>
+          <br></br>
+          <div class="flex justify-evenly items-center max-w-3xl">
+            <button
+              class="bg-emerald-400 w-24 h-10 rounded-3xl text-gray-100 font-semibold uppercase tracking-wide mr-2"
+              onClick={() => vote(3)}
+            >
+              Vote
+            </button>
+            <h2 class="text-2xl font-bold text-slate-900">{votingCount[3]}</h2>
+            <button
+              class="bg-red-500 w-24 h-10 rounded-3xl text-gray-100 font-semibold uppercase tracking-wide ml-2"
+              onClick={() => unvote(3)}
             >
               Unvote
             </button>
@@ -153,11 +194,11 @@ function App() {
         <div class="flex flex-col bg-white font-semibold text-center rounded-3xl shadow-lg p-7 max-w-3xl my-5">
           <img
             class="mb-3 w-48 h-48 rounded-full shadow-lg mx-auto"
-            src="https://sv1.picz.in.th/images/2022/01/29/nXUVB0.jpg"
+            src="https://sv1.picz.in.th/images/2022/02/06/nMBP8Z.jpg"
             alt="product designer"
           ></img>
-          <h1 class="text-lg text-gray-800"> Weeravit Srisuntikanaporn </h1>
-          <h3 class="text-sm text-gray-600 "> ID: 62010838 </h3>
+          <h1 class="text-lg text-gray-800"> Siwat Phrommak </h1>
+          <h3 class="text-sm text-gray-600 "> ID: 62010884 </h3>
           <p class="text-xs text-gray-400 mt-4 max-w-3xl">
             {" "}
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
@@ -172,52 +213,14 @@ function App() {
           <div class="flex justify-evenly items-center max-w-3xl">
             <button
               class="bg-emerald-400 w-24 h-10 rounded-3xl text-gray-100 font-semibold uppercase tracking-wide mr-2"
-              onClick={() => vote(1)}
+              onClick={() => vote(4)}
             >
               Vote
             </button>
-            <h2 class="text-2xl font-bold text-slate-900">{votingCount[1]}</h2>
+            <h2 class="text-2xl font-bold text-slate-900">{votingCount[4]}</h2>
             <button
               class="bg-red-500 w-24 h-10 rounded-3xl text-gray-100 font-semibold uppercase tracking-wide ml-2"
-              onClick={() => unvote(1)}
-            >
-              Unvote
-            </button>
-          </div>
-        </div>
-        {/* end Personal card */}
-
-        {/* Personal card */}
-        <div class="flex flex-col bg-white font-semibold text-center rounded-3xl shadow-lg p-7 max-w-3xl my-5">
-          <img
-            class="mb-3 w-48 h-48 rounded-full shadow-lg mx-auto"
-            src="https://sv1.picz.in.th/images/2022/01/29/nXUVB0.jpg"
-            alt="product designer"
-          ></img>
-          <h1 class="text-lg text-gray-800"> Weeravit Srisuntikanaporn </h1>
-          <h3 class="text-sm text-gray-600 "> ID: 62010838 </h3>
-          <p class="text-xs text-gray-400 mt-4 max-w-3xl">
-            {" "}
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-            facilisis lectus quis dui eleifend egestas. Pellentesque habitant
-            morbi tristique senectus et netus et malesuada fames ac turpis
-            egestas. Sed ac urna venenatis, consectetur elit ac, volutpat ipsum.
-            Aliquam placerat suscipit volutpat. Mauris quis congue diam. Sed at
-            lacus nec est molestie interdum. Donec scelerisque elementum auctor.
-            Integer quis justo accumsan, ullamcorper tellus id, rutrum ipsum.{" "}
-          </p>
-          <br></br>
-          <div class="flex justify-evenly items-center max-w-3xl">
-            <button
-              class="bg-emerald-400 w-24 h-10 rounded-3xl text-gray-100 font-semibold uppercase tracking-wide mr-2"
-              onClick={() => vote(1)}
-            >
-              Vote
-            </button>
-            <h2 class="text-2xl font-bold text-slate-900">{votingCount[1]}</h2>
-            <button
-              class="bg-red-500 w-24 h-10 rounded-3xl text-gray-100 font-semibold uppercase tracking-wide ml-2"
-              onClick={() => unvote(1)}
+              onClick={() => unvote(4)}
             >
               Unvote
             </button>
