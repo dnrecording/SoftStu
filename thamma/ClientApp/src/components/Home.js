1ee3a52d5330294dce6bc3747e9d1c68b67b3916
poksiwakorn <62551895+poksiwakorn@users.noreply.github.com>
2022-04-09 22:11:13 +0700

    Yeaheah

--- thamma/ClientApp/src/components/Home.js
+++ thamma/ClientApp/src/components/Home.js
@@ -1,26 +1,36 @@
 import React, { Component } from 'react';
+import './Home.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Card} from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 
 export class Home extends Component {
   static displayName = Home.name;
 
   render () {
+    const contentCard = (
+      <Card className='contentCard'>
+        <div className='row no-gutters'></div>
+        <Card.Img className='contentImage' variant="top" src="temple1.jpg" />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the bulk of
+            the card's content.
+          </Card.Text>
+          <Button variant="primary">more details...</Button>
+        </Card.Body>
+      </Card>)
     return (
-      <div>
-        <h1>Hello, world!</h1>
-        <p>Welcome to your new single-page application, built with:</p>
-        <ul>
-          <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
-          <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
-          <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
-        </ul>
-        <p>To help you get started, we have also set up:</p>
-        <ul>
-          <li><strong>Client-side navigation</strong>. For example, click <em>Counter</em> then <em>Back</em> to return here.</li>
-          <li><strong>Development server integration</strong>. In development mode, the development server from <code>create-react-app</code> runs in the background automatically, so your client-side resources are dynamically built on demand and the page refreshes when you modify any file.</li>
-          <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
-        </ul>
-        <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
+      <div className="App">
+      <h2 className="app-header">THAMMA</h2>
+      <div className="app-grid">
+        <p>{contentCard}</p>
+        <p>{contentCard}</p>
+        <p>{contentCard}</p>
+        <p>{contentCard}</p>
       </div>
+    </div>
     );
   }
 }
