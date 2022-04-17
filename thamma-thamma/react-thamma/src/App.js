16f128d2463305ababc1e3dbcfba53cbb6c7eec7
drgn <62900543+dnrecording@users.noreply.github.com>
2022-04-17 16:41:43 +0700

    refactor react

--- thamma-thamma/react-thamma/src/App.js
+++ thamma-thamma/react-thamma/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Route, Routes } from "react-router";
+import { BrowserRouter as Router } from "react-router-dom";
+import Profile from "./Profile";
+import OtherProfile from "./OtherProfile";
+import HomePage from "./HomePage";
+import ContentPage from "./ContentPage";
+import LoginPage from "./LoginPage";
+import RegisterPage from "./RegisterPage";
+import CreatePostPage from "./CreatePostPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <Routes>
+          <Route exact path="/" element={<HomePage />}></Route>
+          <Route exact path="/profile" element={<Profile />}></Route>
+          <Route path="/profile/:id" element={<OtherProfile />}></Route>
+          <Route path="/content" element={<ContentPage />}></Route>
+          <Route path="/login" element={<LoginPage />}></Route>
+          <Route path="/register" element={<RegisterPage />}></Route>
+          <Route path="/createpost" element={<CreatePostPage />}></Route>
+        </Routes>
+      </Router>
   );
 }
 
