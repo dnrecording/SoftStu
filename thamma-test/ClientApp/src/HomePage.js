fb070c3790e83f93c1ba2f5e72f6421f04a2d605
poksiwakorn <62551895+poksiwakorn@users.noreply.github.com>
2022-04-20 13:40:39 +0700

    ContentPageV1

--- thamma-test/ClientApp/src/HomePage.js
+++ thamma-test/ClientApp/src/HomePage.js
@@ -1,18 +1,40 @@
 import React, { useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
-import { Modal, Button } from 'react-bootstrap';
+import { Modal, Button, Card } from 'react-bootstrap';
 import { useNavigate } from "react-router";
 import axios from "axios";
-import "./Profile.css";
+import "./HomePage.css";
 import { Layout } from "./components/Layout";
 
 function HomePage() {
     const navigate = useNavigate();
 
+    const contentCard = (
+        <Card className='contentCard'>
+          <div className='row no-gutters'></div>
+          <div class="col-auto">
+            <img className='contentImage' src="temple1.jpg" />
+          </div>
+          <div class="col">
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the bulk of
+                the card's content.
+              </Card.Text>
+              <Button class="btn btn-primary mx-2" onClick={() => {navigate("/content")}}>more details...</Button>
+              <Button className='like-button'>Like</Button>
+            </Card.Body>
+          </div>
+        </Card>
+        )
+
     return (
         <Layout>
-            <h1>Home Page</h1>
-            <Button class="btn btn-primary mx-2" onClick={() => {navigate("/content")}}>Go to Content</Button>
+            <p>{contentCard}</p>
+            <p>{contentCard}</p>
+            <p>{contentCard}</p>
+            <p>{contentCard}</p>
         </Layout>
     );
 }
