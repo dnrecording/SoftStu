a621850722fc9d1ca451d84e1418ddeae7a8755a
Pubodin <oatxshot555@gmail.com>
2022-04-22 08:07:45 +0700

    HomePageV1

--- thamma-thamma/react-thamma/src/HomePage.js
+++ thamma-thamma/react-thamma/src/HomePage.js
@@ -1,51 +1,37 @@
 import React, { useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
-import { Modal, Button } from "react-bootstrap";
+import { Modal, Button, Card } from "react-bootstrap";
 import { useNavigate } from "react-router";
 import axios from "axios";
 import "./Profile.css";
 import { Layout } from "./components/Layout";
+import ContentCard from "./components/ContentCard";
+
+const client = axios.create({
+  baseURL: "https://localhost:7290/api/post/",
+});
 
 function HomePage() {
-  const navigate = useNavigate();
-  let currentuser = localStorage.getItem("id");
-  const url = `https://localhost:7290/api/user/${currentuser}`;
-
-  const [currentUser, setCurrentUser] = useState("");
-  // in case need to use current user data
-  // [0]: id, [1]: username, [2]: password, [3]: email, [4]: fname, [5]: lname, [6]: img, [7]: status
+  const [post, setPost] = useState({});
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    (async () => {
-      if (currentuser !== null) {
-        const users = await axios.get(url);
-        setCurrentUser(Object.values(users.data));
-      } else {
-        navigate("/login");
-      }
-    })();
+    async function getPost() {
+      const response = await client.get("");
+      setPost(response.data);
+    }
+    getPost();
   }, []);
-
+  if (error) return `Error: ${error.message}`;
+  if (!post) return "No post!";
   return (
     <Layout>
       <h1>Home Page</h1>
-      <p>{currentuser}</p>
-      <Button
-        class="btn btn-primary mx-2"
-        onClick={() => {
-          navigate("/content");
-        }}
-      >
-        Go to Content
-      </Button>
-      {/* testing */}
-<br></br>
-<div class="text-center">
-<Button class="btn btn-primary mx-2" onClick={() => {navigate("/profile/62526df6d30be6196cd5f864")}}>admin</Button>
-<Button class="btn btn-primary mx-2" onClick={() => {navigate("/profile/62527dd6d30be6196cd5f865")}}>user1</Button>
-<Button class="btn btn-primary mx-2" onClick={() => {navigate("/profile/62527e2cd30be6196cd5f866")}}>user2</Button>
-<Button class="btn btn-primary mx-2" onClick={() => {navigate("/profile/625a9e738f59ed05920b6444")}}>user3</Button>
-</div>
+      {post && post.length
+        ? post.map((item, i) => (
+          <ContentCard  id ={item.id} title = {item.title} content = {item.content} img = {item.img}/>
+          ))
+        : ""}
     </Layout>
   );
 }
