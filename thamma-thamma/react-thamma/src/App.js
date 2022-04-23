0d849747ba3275b859fb3f883f60e1fafa7a092e
Siwat Phrommak <77423738+Siwat-2402@users.noreply.github.com>
2022-04-24 02:17:23 +0700

    Edit

--- thamma-thamma/react-thamma/src/App.js
+++ thamma-thamma/react-thamma/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Route, Routes, Switch } from "react-router";
+import { Route, Routes } from "react-router";
 import { BrowserRouter as Router } from "react-router-dom";
 import Profile from "./Profile";
 import OtherProfile from "./OtherProfile";
@@ -9,35 +9,25 @@ import LoginPage from "./LoginPage";
 import RegisterPage from "./RegisterPage";
 import CreatePostPage from "./CreatePostPage";
 import UserTable from "./components/UserTable";
-import { CSSTransition, TransitionGroup } from "react-transition-group";
+import {
+  CSSTransition,
+  TransitionGroup,
+}from 'react-transition-group';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route
-          render={(location) => (
-            <TransitionGroup>
-              <CSSTransition key={location.key} timeout={3000} classNames="fade">
-                <Switch location={location}>
-                  <Route exact path="/" element={<HomePage />}></Route>
-                  <Route exact path="/profile" element={<Profile />}></Route>
-                  <Route path="/profile/:id" element={<OtherProfile />}></Route>
-                  <Route path="/content" element={<ContentPage />}></Route>
-                  <Route path="/login" element={<LoginPage />}></Route>
-                  <Route path="/register" element={<RegisterPage />}></Route>
-                  <Route
-                    path="/createpost"
-                    element={<CreatePostPage />}
-                  ></Route>
-                  <Route path="/manage" element={<UserTable />}></Route>
-                </Switch>
-              </CSSTransition>
-            </TransitionGroup>
-          )}
-        />
-      </Routes>
-    </Router>
+        <Routes>
+          <Route exact path="/" element={<HomePage />}></Route>
+          <Route exact path="/profile" element={<Profile />}></Route>
+          <Route path="/profile/:id" element={<OtherProfile />}></Route>
+          <Route path="/content" element={<ContentPage />}></Route>
+          <Route path="/login" element={<LoginPage />}></Route>
+          <Route path="/register" element={<RegisterPage />}></Route>
+          <Route path="/createpost" element={<CreatePostPage />}></Route>
+          <Route path="/manage" element={<UserTable />}></Route>
+        </Routes>
+      </Router>
   );
 }
 
