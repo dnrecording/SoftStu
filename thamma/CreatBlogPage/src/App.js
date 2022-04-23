1ae364ca02aa64f4f4253ffccba51cf2dae64430
kuroisepr <kanittha41@gmail.com>
2022-04-24 02:14:42 +0700

    CreateBlog -> Bug = True

--- thamma/CreatBlogPage/src/App.js
+++ thamma/CreatBlogPage/src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import {Button, Form, Input,Upload, Tooltip} from 'antd'
-import './App.css';
-import {PlusCircleOutlined} from '@ant-design/icons'
-import { text } from '@fortawesome/fontawesome-svg-core';
-import TextArea from 'antd/lib/input/TextArea';
 import TopBar from './AppHeader'
+import WritePost from './WritePost'
 
 function App () {
   return(
-    <TopBar/>
+    <>
+      <TopBar />
+      <WritePost /> 
+    </>
   )
 }
 
--- thamma/CreatBlogPage/src/WritePost.jsx
+++ thamma/CreatBlogPage/src/WritePost.jsx
@@ -1,40 +1,55 @@
 import React from 'react';
 import {Button, Form, Input,Upload, Tooltip} from 'antd'
-import './App.css';
+import './WritePost.css';
 import {PlusCircleOutlined} from '@ant-design/icons'
 import TextArea from 'antd/lib/input/TextArea';
 
-const App = () => {
-    return(
-      <div className = "write">
-        <form className='writeForm'>
-          <div className = 'writeFormGroup'>
-            <Form.Item name={['Title']}>
-                <Upload name="logo" action="/upload.do">
-                  <Tooltip title = "Picture" placement='bottom'>
-                  <Button icon={<PlusCircleOutlined/>} 
-                    type={'text'} 
-                    id={'fileInput'} 
-                    style={{left: '55px', top: '93px'}}>
-                  </Button> 
-                  </Tooltip>
-                  <Tooltip title = 'Link' placement='bottom'>
-                  <Button icon={<PlusCircleOutlined/>} 
-                    type={'text'} 
-                    id={'fileInput'} 
-                    style={{top: '130px', left: '23px'}}>
-                  </Button>
-                  </Tooltip> 
-                </Upload>
-                <Input style={{width: '20%', left: '100px'}} placeholder="Title" size="large" bordered={false} maxLength={30} />
-            </Form.Item>
-          </div>
-          <div className='writeFormGroup'>
-            <TextArea rows={10} placeholder="Tell your story.." bordered={false}
-              style = {{width: '85%', left:'100px'}} />
-          </div>
-          <Button className='writeSubmit' type='primary' shape='round' size='large'>Submit</Button>
-        </form>
-      </div>
-    );
+export default function WritePost() {
+
+  const onFinish = values => {
+    console.log('Success:',values);
   };
+  const onFinishFailed = errorInfo => {
+    console.log('Failed:', errorInfo);
+  };
+
+  return(
+    <div className = "write">
+        <div className = 'writeFormGroup'>
+          <Form name= "head" 
+                onFinish={onFinish}
+                onFinishFailed={onFinishFailed}
+                autoComplete = "off">
+              <Upload name="logo" action="/upload.do">
+                <Tooltip title = "Picture" placement='bottom'>
+                <Button icon={<PlusCircleOutlined/>} 
+                  type={'text'} 
+                  id={'fileInput'} 
+                  style={{left: '55px', top: '93px'}}>
+                </Button> 
+                </Tooltip>
+                <Tooltip title = 'Link' placement='bottom'>
+                <Button icon={<PlusCircleOutlined/>} 
+                  type={'text'} 
+                  id={'fileInput'} 
+                  style={{top: '130px', left: '23px'}}>
+                </Button>
+                </Tooltip> 
+              </Upload>
+              <Form.Item 
+                name = "title"
+                rules={[{ required: true, message: 'Please Enter Title'}]} 
+                requiredMark='hidden'
+                >
+                <Input style={{width: '20%', left: '100px'}} placeholder="Title" size="large" bordered={false} maxLength={30} />
+              </Form.Item>
+          </Form>
+        </div>
+        <div className='writeFormGroup'>
+          <TextArea rows={10} placeholder="Tell your story.." bordered={false}
+            style = {{width: '85%', left:'100px'}} />
+        </div>
+        <Button className='writeSubmit' type='primary' shape='round' size='large' htmlType='submit'>Submit</Button>
+    </div>
+  )
+}
