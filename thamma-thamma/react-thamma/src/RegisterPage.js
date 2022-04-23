5b0b98533f05426468d31575b591806354913005
Siwat Phrommak <77423738+Siwat-2402@users.noreply.github.com>
2022-04-24 00:11:45 +0700

    Nav_Login_Profile_Register

--- thamma-thamma/react-thamma/src/RegisterPage.js
+++ thamma-thamma/react-thamma/src/RegisterPage.js
@@ -24,7 +24,6 @@ function RegisterPage() {
     if (userCreated.status === 201) {
       navigate("/login");
     }
-    // console.log(userCreated)
   }
 
   const onSubmit = (data) => {
@@ -34,131 +33,131 @@ function RegisterPage() {
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <div className="container">
-        <div className="row d-flex justify-content-center">
-          <div className="col-md-4">
-            <Card className="card-size">
-              <div classNameNameName="App">
-                <div className="container">
-                  <div className="row d-flex justify-content-center">
-                    <div className="col-md-4"></div>
-                    <h3 className="mt-3">Register</h3>
+        <div className="container">
+          <div className="row d-flex justify-content-center">
+            <div className="col-md-4">
+              <Card className="card-size">
+                <div classNameNameName="App">
+                  <div className="container">
+                    <div className="row d-flex justify-content-center">
+                      <div className="col-md-4"></div>
+                      <h3 className="mt-3 mb-4"><strong>Register </strong></h3>
 
-                    <div className="form-group">
-                      <label>Username</label>
-                      <input
-                        type="text"
-                        className="form-control"
-                        placeholder="Enter username"
-                        {...register("username", {
-                          required: true,
-                          maxLength: {
-                            value: 20,
-                            message: "this need to be max length of 20",
-                          },
-                          minLength: {
-                            value: 4,
-                            message: "this need to be min length of 4",
-                          },
-                        })}
-                      />
-                      <p class="text-danger">{errors.username?.message}</p>
+                      <div className="form-group">
+                        <label>Username</label>
+                        <input
+                          type="text"
+                          className="form-control"
+                          placeholder="Enter username"
+                          {...register("username", {
+                            required: true,
+                            maxLength: {
+                              value: 20,
+                              message: "this need to be max length of 20",
+                            },
+                            minLength: {
+                              value: 4,
+                              message: "this need to be min length of 4",
+                            },
+                          })}
+                        />
+                        <p class="text-danger">{errors.username?.message}</p>
+                      </div>
+
+                      <div className="form-group">
+                        <label>Password</label>
+                        <input
+                          type="password"
+                          className="form-control"
+                          placeholder="Enter password"
+                          {...register("password", {
+                            required: true,
+                            maxLength: {
+                              value: 20,
+                              message: "this need to be max length of 20",
+                            },
+                            minLength: {
+                              value: 8,
+                              message: "this need to be min length of 4",
+                            },
+                          })}
+                        />
+                        <p class="text-danger">{errors.password?.message}</p>
+                      </div>
+
+                      <div className="form-group">
+                        <label>Email</label>
+                        <input
+                          type="email"
+                          className="form-control"
+                          placeholder="Enter email"
+                          {...register("email", {
+                            required: true,
+                            pattern:
+                              /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                          })}
+                        />
+                        <p class="text-danger">{errors.email?.message}</p>
+                      </div>
+
+                      <div className="form-group">
+                        <label>First name</label>
+                        <input
+                          type="text"
+                          className="form-control"
+                          placeholder="First name"
+                          {...register("fname", {
+                            required: true,
+                            maxLength: {
+                              value: 20,
+                              message: "this need to be max length of 20",
+                            },
+                            minLength: {
+                              value: 4,
+                              message: "this need to be min length of 4",
+                            },
+                          })}
+                        />
+                        <p class="text-danger">{errors.fname?.message}</p>
+                      </div>
+
+                      <div className="form-group">
+                        <label>Last name</label>
+                        <input
+                          type="text"
+                          className="form-control"
+                          placeholder="Last name"
+                          {...register("lname", {
+                            required: true,
+                            maxLength: {
+                              value: 20,
+                              message: "this need to be max length of 20",
+                            },
+                            minLength: {
+                              value: 4,
+                              message: "this need to be min length of 4",
+                            },
+                          })}
+                        />
+                        <p class="text-danger">{errors.lname?.message}</p>
+                      </div>
+
+                      <button
+                        type="submit"
+                        className="btn btn-dark btn-lg btn-block mb-3 mt-3"
+                      >
+                        Register
+                      </button>
+                      <p className="forgot-password text-right">
+                        Already registered <a href="login">log in?</a>
+                      </p>
                     </div>
-
-                    <div className="form-group">
-                      <label>Password</label>
-                      <input
-                        type="password"
-                        className="form-control"
-                        placeholder="Enter password"
-                        {...register("password", {
-                          required: true,
-                          maxLength: {
-                            value: 20,
-                            message: "this need to be max length of 20",
-                          },
-                          minLength: {
-                            value: 8,
-                            message: "this need to be min length of 4",
-                          },
-                        })}
-                      />
-                      <p class="text-danger">{errors.password?.message}</p>
-                    </div>
-
-                    <div className="form-group">
-                      <label>Email</label>
-                      <input
-                        type="email"
-                        className="form-control"
-                        placeholder="Enter email"
-                        {...register("email", {
-                          required: true,
-                          pattern:
-                            /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-                        })}
-                      />
-                      <p class="text-danger">{errors.email?.message}</p>
-                    </div>
-
-                    <div className="form-group">
-                      <label>First name</label>
-                      <input
-                        type="text"
-                        className="form-control"
-                        placeholder="First name"
-                        {...register("fname", {
-                          required: true,
-                          maxLength: {
-                            value: 20,
-                            message: "this need to be max length of 20",
-                          },
-                          minLength: {
-                            value: 4,
-                            message: "this need to be min length of 4",
-                          },
-                        })}
-                      />
-                      <p class="text-danger">{errors.fname?.message}</p>
-                    </div>
-
-                    <div className="form-group">
-                      <label>Last name</label>
-                      <input
-                        type="text"
-                        className="form-control"
-                        placeholder="Last name"
-                        {...register("lname", {
-                          required: true,
-                          maxLength: {
-                            value: 20,
-                            message: "this need to be max length of 20",
-                          },
-                          minLength: {
-                            value: 4,
-                            message: "this need to be min length of 4",
-                          },
-                        })}
-                      />
-                      <p class="text-danger">{errors.lname?.message}</p>
-                    </div>
-
-                    <button
-                      type="submit"
-                      className="btn btn-dark btn-lg btn-block mb-2"
-                    >
-                      Register
-                    </button>
-                    <p className="forgot-password text-right">
-                      Already registered <a href="login">log in?</a>
-                    </p>
                   </div>
                 </div>
-              </div>
-            </Card>
+              </Card>
+            </div>
           </div>
         </div>
-      </div>
     </form>
   );
 }
