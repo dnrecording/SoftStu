0f6b88a6e585f14bbecaedd604824a368e3defe5
kuroisepr <kanittha41@gmail.com>
2022-04-24 14:29:15 +0700

    Bug fixed

--- thamma/CreatBlogPage/src/WritePost.jsx
+++ thamma/CreatBlogPage/src/WritePost.jsx
@@ -14,42 +14,41 @@ export default function WritePost() {
   };
 
   return(
-    <div className = "write">
-        <div className = 'writeFormGroup'>
-          <Form name= "head" 
-                onFinish={onFinish}
-                onFinishFailed={onFinishFailed}
-                autoComplete = "off">
-              <Upload name="logo" action="/upload.do">
-                <Tooltip title = "Picture" placement='bottom'>
-                <Button icon={<PlusCircleOutlined/>} 
-                  type={'text'} 
-                  id={'fileInput'} 
-                  style={{left: '55px', top: '93px'}}>
-                </Button> 
-                </Tooltip>
-                <Tooltip title = 'Link' placement='bottom'>
-                <Button icon={<PlusCircleOutlined/>} 
-                  type={'text'} 
-                  id={'fileInput'} 
-                  style={{top: '130px', left: '23px'}}>
-                </Button>
-                </Tooltip> 
-              </Upload>
-              <Form.Item 
-                name = "title"
-                rules={[{ required: true, message: 'Please Enter Title'}]} 
-                requiredMark='hidden'
-                >
-                <Input style={{width: '20%', left: '100px'}} placeholder="Title" size="large" bordered={false} maxLength={30} />
-              </Form.Item>
-          </Form>
-        </div>
-        <div className='writeFormGroup'>
-          <TextArea rows={10} placeholder="Tell your story.." bordered={false}
-            style = {{width: '85%', left:'100px'}} />
-        </div>
-        <Button className='writeSubmit' type='primary' shape='round' size='large' htmlType='submit'>Submit</Button>
-    </div>
+    <Form name = "head"
+    onFinish={onFinish}
+    onFinishFailed={onFinishFailed}
+    autoComplete = "off" 
+    labelCol={{ span: 8 }}
+    wrapperCol={{ span: 16 }}>
+      <Upload name="logo" action="/upload.do">
+          <Tooltip title = "Picture" placement='bottom'>
+            <Button icon={<PlusCircleOutlined/>} 
+              type={'text'} 
+              id={'fileInput'} 
+              style={{left: '55px', top: '103px'}}>
+            </Button> 
+          </Tooltip>
+          <Tooltip title = 'Link' placement='bottom'>
+            <Button icon={<PlusCircleOutlined/>} 
+              type={'text'} 
+              id={'fileInput'} 
+              style={{top: '140px', left: '23px'}}>
+            </Button>
+          </Tooltip> 
+      </Upload>
+      <Form.Item className="TitleText"
+        name = "title" size = "Large" maxLength = {30}
+        rules={[{ required: true, message: 'Please Enter Title'}]} >
+        <Input placeholder="Title" size="large" bordered={false} maxLength={30} />
+      </Form.Item>
+      <Form.Item className = "storyText"
+      name = "story"
+      rules={[{ required: true, message: 'Please Enter Your Messages' }]}>
+        <TextArea rows={15} placeholder="Tell your story.." bordered={false} />  
+      </Form.Item>
+      <Button type = 'primary' shape = 'round'className="submitButton" htmlType='submit'>
+        Submit
+      </Button>
+    </Form>
   )
 }
